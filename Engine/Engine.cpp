f69fbc0e501c48c52fd48efed7229c42a6d1e7de
Snowblaze <a.a.matinyan@gmail.com>
2020-04-01 11:07:10 +0400

    Add Vulkan, GLFW and GLM. Create a window with glfw.

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -1,5 +1,14 @@
 ﻿// Engine.cpp : Defines the entry point for the application.
 //
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
 
 #include "Engine.h"
 #include "EngineConfig.h"
@@ -34,6 +43,32 @@ microseconds getTimeSinceStart()
 
 int main()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Engine window", nullptr, nullptr);
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	cout << extensionCount << " extensions supported" << endl;
+
+	glm::mat4 matrix;
+	glm::vec4 vec;
+	auto test = matrix * vec;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
+	return 0;
+
+
+
 	microseconds lag(0);
 	microseconds previous = getTimeSinceStart();
 	int frame = 0;
